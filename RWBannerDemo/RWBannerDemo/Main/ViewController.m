c53823ae9e023895e81fe16cd5c72852ec639ea5
hnbwyh <rainwang@hinabian.com>
2018-05-17 16:29:20 +0800

    完善  Demo

--- RWBannerDemo/RWBannerDemo/Main/ViewController.m
+++ RWBannerDemo/RWBannerDemo/Main/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "ScrollBannerView.h"
 
-@interface ViewController ()
+@interface ViewController ()<ScrollBannerViewDelegate>
+
+@property (nonatomic,strong) ScrollBannerView *yhbanner;
 
 @end
 
@@ -16,13 +19,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _yhbanner = [[ScrollBannerView alloc] initWithFrame:CGRectMake(0,
+                                                                   0,
+                                                                   [UIScreen mainScreen].bounds.size.width,
+                                                                   174)
+                                   autoPlayTimeInterval:5];
+    _yhbanner.delegate = self;
+    [self.view addSubview:_yhbanner];
+    [_yhbanner reFreshBannerViewWithDataSource:@[@"image_1.png",@"image_2.png",@"image_3.png",@"image_4.png",@"image_5.png"]];
+
+    
 }
 
+#pragma mark ------ ScrollBannerViewDelegate
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)scrollBannerView:(ScrollBannerView *)banner didSelectedImageViewAtIndex:(NSInteger)index{
+    NSLog(@" 999 ------ > %ld",(long)index);
 }
 
 
